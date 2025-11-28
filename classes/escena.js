58f3fd150aad43e64fac19d6e84ea3a458276e03
Fay <fabilagosver@gmail.com>
2025-11-27 21:57:22 -0300

    Delete classes/escena.js

--- classes/escena.js
+++
@@ -1,6 +0,0 @@
-// =========================
-// classes/escena.js (ES5)
-// =========================
-function Escena(){ this.game=new Carrera(); }
-Escena.prototype.update=function(){ this.game.update(); };
-Escena.prototype.draw=function(){ this.game.draw(); };
